91a93346cb14c2141a33e85987681dcc80ce8bb7
Jannik Lorenz <dev@janniklorenz.de>
2020-05-17 22:53:24 +0200

    add offline message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { DscAPI_Token, DscAPIInterface } from "./views/dsc/api";
     BrowserModule,
     AppRoutingModule,
     ViewsModule,
+    FlexLayoutModule,
   ],
   providers: [
     { provide: DscAPI_Token, useExisting: DscApiService},
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,29 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// import { NavbarsModule } from './navbars/navbars.module';
-// import { InfoModule } from './info/info.module';
-// import { DscComponent } from './dsc/dsc.component';
-// import { DscPrintComponent } from './dsc-print/dsc-print.component';
-// import { TargetModule } from './target/target.module';
 import { DscModule } from './dsc/dsc.module';
 
 @NgModule({
-  declarations: [
-    // DscComponent,
-    // DscPrintComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule,
-    // FlexLayoutModule,
-    // BrowserAnimationsModule,
-    // NavbarsModule,
-    // InfoModule,
-    // TargetModule,
   ],
   exports: [
-    // DscComponent,
-    // DscPrintComponent,
     DscModule,
   ],
 })
